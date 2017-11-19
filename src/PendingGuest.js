6f6f23e864c257e54651093522183de1b396f6f3
Sabrina John <sabrina.john1990@gmail.com>
2017-11-19 13:39:22 -0500

    added pendingGuest props to Guestlist component and app

--- src/PendingGuest.js
+++ src/PendingGuest.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const PendingGuest = props =>
-      <li className="pending">
-        <span>
-          {props.name}
-        </span>
-      </li>;
+const PendingGuest = props => {
+  if (props.name) {
+      return (
+        <li className="pending">
+          <span>
+            {props.name}
+          </span>
+        </li>
+      );
+    }
+    return null
+}
+
 
 PendingGuest.propTypes = {
   name: PropTypes.string.isRequired,
